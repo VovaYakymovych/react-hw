44e6a5ef5bd522a97808c17b8fd84e7b89ae540b
VovaYakymovych <yakym.volodya@gmail.com>
2025-03-31 03:13:45 +0300

    file delete from t1.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 
 import './App.css'
-import FamilyComponent from "./MyComponents/FamilyComponent/FamilyComponent.tsx";
+
 
 function App() {
 
   return (
-    <>
-      <FamilyComponent/>
-    </>
+    <></>
   )
 }
 
--- src/models/simpsonsArrayType.ts
+++
@@ -1,7 +0,0 @@
-export type simpsonsArrayType = {
-  name: string;
-  surname: string;
-  age: number;
-  info: string;
-  photo: string;
-}
