0e059c4beaeb45d1e689cb6b13bd8ee3aa4ff34f
VovaYakymovych <yakym.volodya@gmail.com>
2025-04-06 00:13:49 +0300

    hw 2.2upd

--- src/services/api-service-functions.ts
+++ src/services/api-service-functions.ts
@@ -1,7 +1,9 @@
 import {PostsType} from "../models/PostsType.ts";
 
+export const endpoint = import.meta.env.VITE_API_JSONPLACEHOLDER_URL
+
 export const getPosts = async (): Promise<PostsType[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/posts')
+    return await fetch(endpoint+'/posts')
         .then(value => value.json());
     ;
 }
