19f0360a339dd4ccf17c3dab258fd5b3f9d74bcd
VovaYakymovych <yakym.volodya@gmail.com>
2025-04-06 19:56:44 +0300

    hw 2.5 upd

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,17 +1,22 @@
-import {FC} from 'react';
-import {CommentsType, UserType} from "../../models/dummyJsonCommentsResponseType.ts";
-import './comment-style.css'
+import { FC, useState } from 'react';
+import { CommentsType, UserType } from "../../models/dummyJsonCommentsResponseType.ts";
+import './comment-style.css';
 
 type CommentPropsType = {
     item: CommentsType
-}
+};
 
-const Comment: FC<CommentPropsType> = ({item}) => {
+const Comment: FC<CommentPropsType> = ({ item }) => {
+    const user: UserType = item.user;
 
-    const user:UserType = item.user
+
+    const [likes, setLikes] = useState<number>(item.likes);
+    const like = () => {
+        setLikes(prev => prev + 1);
+        console.log('like');
+    };
 
     return (
-
         <div className={'comment'}>
             <h2 className={'body'}>{item.body}</h2>
             <p className={'ids'}>Comment ID: {item.id} | Post ID: {item.postId}</p>
@@ -20,7 +25,9 @@ const Comment: FC<CommentPropsType> = ({item}) => {
                 <p className={'user-fullName'}>{user.fullName}</p>
                 <p className={'userId'}>User`s ID: {user.id}</p>
             </div>
-            <p className={'likes'}>👍 {item.likes}</p>
+            <p className={'likes'} onClick={like}>
+                👍 {likes}
+            </p>
         </div>
     );
 };
