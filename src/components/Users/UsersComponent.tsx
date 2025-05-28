1aa84475af047742a814787a9e61e9e0195e0245
VovaYakymovych <yakym.volodya@gmail.com>
2025-05-28 19:53:33 +0300

    hw4

--- src/components/Users/UsersComponent.tsx
+++ src/components/Users/UsersComponent.tsx
@@ -1,10 +1,20 @@
+import {useEffect, useState} from "react";
+import {getUsers} from "../../services/service.api.ts";
+import {IUser} from "../../models/IUser.ts";
 
 const UsersComponent = () => {
-    return (
-        <div>
+    const [users,setUsers] = useState<IUser[]>([])
+    useEffect(()=>{
+        getUsers()
+            .then(response => setUsers(response))
+    },[])
 
+    return (
+        <div className={'api-res-component'}>
+            {users.map(value => <UsersComponent user={value} key={value.id}/>)}
         </div>
-    );
+    )
+};
 };
 
 export default UsersComponent;
