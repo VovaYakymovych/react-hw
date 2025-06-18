b2335eadce2c5d484e11ef7e3fd3dbc5377ea2c4
VovaYakymovych <yakym.volodya@gmail.com>
2025-06-18 18:08:15 +0300

    hw6 design login page

--- src/components/LoginConponent/LoginComponent.tsx
+++ src/components/LoginConponent/LoginComponent.tsx
@@ -2,15 +2,16 @@ import {joiResolver} from "@hookform/resolvers/joi";
 import {useForm} from "react-hook-form";
 import {loginValidator} from "../../validators/LoginFormSubmit.tsx";
 import {ILoginInputData} from "../../models/ILoginInputData.ts";
+import './LoginComponent.css'
 
 const LoginComponent = () => {
 
-    const {handleSubmit, register, formState:{errors}} = useForm<ILoginInputData>({
-        mode:"all",
-        resolver:joiResolver(loginValidator)
+    const {handleSubmit, register, formState: {errors}} = useForm<ILoginInputData>({
+        mode: "all",
+        resolver: joiResolver(loginValidator)
     });
 
-    const LoginFormSubmit = (data:ILoginInputData) => {
+    const LoginFormSubmit = (data: ILoginInputData) => {
 
         const loginData = {
             username: data.username,
@@ -25,10 +26,16 @@ const LoginComponent = () => {
         <div className={'loginFormWrapper'}>
             <form className={'loginForm'} onSubmit={handleSubmit(LoginFormSubmit)}>
 
-                <input type="text" {...register("username")} placeholder="Username"/>
-                <div>{errors.username?.message}</div>
-                <input type="text" {...register("password")} placeholder="Password"/>
-                <div>{errors.password?.message}</div>
+                <div className={'inputDivWrapper'}>
+                    <div className={'errorDiv'}>{errors.username?.message}</div>
+                    <input type="text" {...register("username")} placeholder="Username"/>
+                </div>
+
+                <div className={'inputDivWrapper'}>
+                    <div className={'errorDiv'}>{errors.password?.message}</div>
+                    <input type="password" {...register("password")} placeholder="Password"/>
+                </div>
+
                 <button>Log in</button>
             </form>
 
